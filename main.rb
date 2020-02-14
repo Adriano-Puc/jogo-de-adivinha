e74662c3f719ccf50fae0aa3e9926d4df620b0a9
Adriano De Oliveira <PUCMINAS\931861@PPC01INFLL00129.pucminas.net>
2020-02-13 21:56:17 -0300

    #1: Tres tentativas implementada.

--- main.rb
+++ main.rb
@@ -4,5 +4,20 @@ palpite = gets.chomp.to_i
 numero_secreto = 121
 
 puts "Voce acertou?"
-
 puts palpite == numero_secreto
+
+if palpite != numero_secreto
+    
+    puts "Entre com um palpite: "
+    palpite = gets.chomp.to_i
+    puts "Voce acertou?"
+    puts palpite == numero_secreto
+end
+
+if palpite != numero_secreto
+    
+    puts "Entre com um palpite: "
+    palpite = gets.chomp.to_i
+    puts "Voce acertou?"
+    puts palpite == numero_secreto
+end
