60b855b2ec77f2af135b51916e97a1b076cc4556
Adriano De Oliveira <PUCMINAS\931861@PPC01INFLL00131.pucminas.net>
2020-02-20 22:06:51 -0300

    #2: Estrutura de repeticao.

--- main.rb
+++ main.rb
@@ -1,23 +1,13 @@
-puts "Entre com um palpite: "
-palpite = gets.chomp.to_i
 
 numero_secreto = 121
+bloqueio = 0
 
-puts "Voce acertou?"
-puts palpite == numero_secreto
-
-if palpite != numero_secreto
-    
-    puts "Entre com um palpite: "
-    palpite = gets.chomp.to_i
-    puts "Voce acertou?"
-    puts palpite == numero_secreto
-end
-
-if palpite != numero_secreto
-    
-    puts "Entre com um palpite: "
-    palpite = gets.chomp.to_i
-    puts "Voce acertou?"
-    puts palpite == numero_secreto
+3.times do    
+    if bloqueio == 0
+        puts "Entre com um palpite: "
+        palpite = gets.chomp.to_i
+        puts "Voce acertou?"
+        puts palpite == numero_secreto
+        bloqueio = 1 if palpite == numero_secreto 
+    end
 end
